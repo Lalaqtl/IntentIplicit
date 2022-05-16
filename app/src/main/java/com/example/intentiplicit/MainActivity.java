93320d37d6f3daa95bae563e6d038cbbe76cdcce
Rhory1 <rlimaticona@gmail.com>
2022-05-16 12:56:27 -0400

    CONFIGURAR PERMISOS ANDROID CLASE18-05

--- app/src/main/java/com/example/intentiplicit/MainActivity.java
+++ app/src/main/java/com/example/intentiplicit/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.intentiplicit;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     //atributos de las vistas
@@ -17,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
     private String numeroTelefono;
 
 
-
+private final int PHONE_CODE=100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +50,11 @@ public class MainActivity extends AppCompatActivity {
 
             //EJ:SDK_INT=24 ES LA VERSIONDE SDK VERSION_CODE.M=23
             if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                //versiones superiores a la 23 se usan funciones de las librerias de Android
+                // e incorporan un asunto llamado asincronia
+                //ASINCRONO= no e necesario esperar que una linea responda para ir a una siguiente lineea
+                requestPermissions(new String[]{Manifest.permission.CALL_PHONE},PHONE_CODE);
+
             }//para versiones nuevas
                 else{
                  //para versiones anmtiguas
@@ -58,6 +67,8 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
+
     private void configurarVersionesAntiguas() {
         //CONFIGURAR EL INTENT PARA VERSIONES ANTERIORES DONDE CAMBIO EL CODIGO
         //Intent implicito->1) Que accion quiere realizar
@@ -65,8 +76,14 @@ public class MainActivity extends AppCompatActivity {
         //lA uri es como la URL e web donde configuras las cabeceras(heathers
         //tu ruta donde tienes que salr atos
         //lavve valor para telefono:
-        Intent intentLlamada=new Intent(Intent.ACTION_CALL, Uri.parse("tel:"+numeroTelefono));
-        startActivity(intentLlamada);
+        if (revisarPermisos(Manifest.permission.CALL_PHONE)){
+            Intent intentLlamada=new Intent(Intent.ACTION_CALL, Uri.parse("tel:"+numeroTelefono));
+            startActivity(intentLlamada);
+        }
+        else {
+            Toast.makeText(this,"Permoisos denegados",Toast.LENGTH_SHORT).show();
+        }
+
     }
 
     private void obtenerInformacion() {
@@ -84,9 +101,31 @@ public class MainActivity extends AppCompatActivity {
         //Nadroid maneeja los permisos de esta manera, para Android GRANTED significa permso otorgado
         // Para android DENIED significa no autorizado
         //VALIDAR si el permiso a evaluar en su aplicacion tiene el valor de Android manja para u permiso otogado (GRANTED)
-        return false;
+        int resultadoPermiso=checkCallingOrSelfPermission(permiso);
+        return resultadoPermiso== PackageManager.PERMISSION_GRANTED;
+
         }
-    /*
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch(requestCode){
+            case PHONE_CODE:
+                String permiso= permissions[0];
+                int valorPermiso=grantResults[0];
+                //PARA EVITAR QUE HAYAN ERRORES HUMANOS MAS QUE NADA
+                if(permiso.equals(Manifest.permission.CALL_PHONE));
+                if(valorPermiso==PackageManager.PERMISSION_GRANTED){
+                    Intent intentLlamada=new Intent(Intent.ACTION_CALL,Uri.parse("tel:"+numeroTelefono));
+                    startActivity(intentLlamada);
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+                break;
+        }
+
+    }
+        /*
     APUNTES 16-05 CLASE INTENT
     IPLICIT-EXPLICIT
     EXPLICIT=indica que componentes se quiere que realice la acciòn
